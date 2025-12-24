a7f5058d80803e43da680ca745a70760486684ff
JYJSXX <95729892+JYJSXX@users.noreply.github.com>
2025-12-24 14:28:23 +0800

    add context

--- nyabot-dev/plugins/shit/shit_plugin.py
+++ nyabot-dev/plugins/shit/shit_plugin.py
@@ -25,30 +25,48 @@ api_key ="5KWYa_nUmeTRLvlgX-reQrgJNI8YRsgd1CFkt0Pbhxc"
 class ShitPluginConfig(CQHTTPGroupMessageCommandHandlerPluginConfig):
     __config_name__ = "schedule"
 
-    
+
+class ShitPluginState(NYAPluginState):
+    def __init__(self) -> None:
+        self.context = []
+    async def from_dict(self, obj)-> None:
+        if obj is None:
+            return
+        self.context = obj.get("context", [])
+    async def to_dict(self) -> dict:
+        return {
+            "context": self.context
+        }
     
 class ShitPlugin(
     CQHTTPGroupMessageCommandHandlerPlugin[
-        GroupMessageEvent, None, ShitPluginConfig
+        GroupMessageEvent, ShitPluginState, ShitPluginConfig
     ]
 ):
-    async def ask_poe(self, message: str) -> ReturnValue:
-        message = "想象你是一个高智商高情商的毒舌人设，请你用尽量简短的纯文本来回复：" + message
+    def __init__(self) -> None:
+        super().__init__()
+        self.state = self.__init_state__()
+    def __init_state__(self) -> ShitPluginState:
+        return ShitPluginState()
+    async def ask_poe(self, *message_: str) -> ReturnValue:
+        message = " ".join(message_)
+        message = "请你用尽量简短的纯文本来回复：" + message
         msg = fp.ProtocolMessage(role="user", content=message)
+        self.state.context.append(msg)
         chunks = ""
-        async for chunk in fp.get_bot_response(messages=[msg], bot_name="GPT-5.2", api_key=api_key):
+        async for chunk in fp.get_bot_response(messages=self.state.context, bot_name="GPT-5.2", api_key=api_key):
             chunks += chunk.text
+        self.state.context.append(fp.ProtocolMessage(role="bot", content=chunks))
         return ReturnValue(0, reply=chunks)
     command = LeafCommand(
         name="ask",
         desc="ask",
         limited_roles=None,
 
-        function=FunctionWithFixedParams(
+        function=FunctionWithVariableParams(
             func=ask_poe,
-            fixed_param_desc_list=[
-                ("message", str, "问题")
-            ]
+            variable_param_desc=("message", str, "要询问的问题"),
+            fixed_param_desc_list=[],
         )
 
     )
